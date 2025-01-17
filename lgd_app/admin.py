dcc20870cfc6d7f09b162f3c39dddffbb0ef8563
Ishita Das <145057695+Msisha@users.noreply.github.com>
2025-01-17 11:24:43 +0530

    Delete lgd_app directory

--- lgd_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, TrainingRequest, Course, Progress, Feedback
-
-admin.site.register(User)
-admin.site.register(TrainingRequest)
-admin.site.register(Course)
-admin.site.register(Progress)
-admin.site.register(Feedback)
